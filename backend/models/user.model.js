36f58ad57a4ef94c6d469293eb97b4991ef031fd
Johan Carlos Herrera <johancarlosherrera@Johans-MacBook-Pro.local>
2025-06-27 23:55:50 -0400

    quinto commit

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,10 +6,17 @@ async function findUserByEmail(email) {
 }
 
 async function createUser({ email, password, name, role }) {
-  await pool.query(
-    'INSERT INTO users (email, password, name, role) VALUES ($1, $2, $3, $4)',
-    [email, password, name, role]
-  );
+  if (role) {
+    await pool.query(
+      'INSERT INTO users (email, password, name, role) VALUES ($1, $2, $3, $4)',
+      [email, password, name, role]
+    );
+  } else {
+    await pool.query(
+      'INSERT INTO users (email, password, name) VALUES ($1, $2, $3)',
+      [email, password, name]
+    );
+  }
 }
 
 module.exports = { findUserByEmail, createUser };
